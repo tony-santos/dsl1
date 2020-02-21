54847559d4df739e2cbe349830ecbd7071e54efc
Tony Santos <santos.tony@gmail.com>
2020-02-21 11:27:37 -0500

    added docstrings to several functinos and added delimiter parameter to convert_df_to_table

--- table_diff2.py
+++ table_diff2.py
@@ -3,40 +3,29 @@ import sys
 import pandas as pd
 import pprint
 
-def get_column_widths(df, column_list):
-    column_widths = []
-    label_lengths = []
-
-    # for i, row in enumerate(df[column_list].itertuples(index=False), 1):
-    # get length of lable for each column
-    for col in column_list:
-        label_lengths.append(len(col))
-
-    # get length of longest value in each column
-    for ix, _ in enumerate(column_list):
-        # column_widths.append(max(len(str(row[ix])) for _, row in enumerate(df[column_list].itertuples(index=False), 1)))    
-        # column_widths.append(max(len(str(row[ix])) for row in df[column_list].itertuples(index=False)))    
-        column_widths.append(max(len(df[column_list].astype(str))))
-
-    # adjust column width if label is longer than all column entries
-    for ix, _ in enumerate(column_widths):
-        column_widths[ix] = max(column_widths[ix], label_lengths[ix])
-
-    return column_widths
-
 def get_longest_entry(df_column):
-    return max(df_column.astype(str).apply(len))
+    """return length of longest entry in a data frame column
 
-def get_column_widths2(df1, df2, column_list):
-    """calculate column widths for a text table
-    sets column width to the longest of the values in either dataframe or the label if lable is longer than any entry
+    converts all entries in column to string format, applies len function on converted column, and returns the max length
     Arguments:
-        df1 {dataframe} -- dataframe of expected data
-        df2 {dataframe} -- dataframe of actual data
-        column_list {list} -- list of columns to be included in text table
+        df_column {dataframe_column (df['col_name'])} -- column of dataframe to be evaluated
     
     Returns:
-        list -- list containing the length of each column
+        [int] -- integer containing length of longest entry (after conversion to string)
+    """
+    return max(df_column.astype(str).apply(len))
+
+def get_column_widths(df1, df2, column_list):
+    """calculate column widths for a text table to be used by difflib for comparison
+
+    sets column width to the longest of the values in either dataframe or the label if label is longer than all entries
+    Arguments:
+        df1 {dataframe} -- dataframe 1
+        df2 {dataframe} -- dataframe 2
+        column_list {list} -- list of columns to be measured
+    
+    Returns:
+        list -- list containing the length of each column in column_list argument
     """
     # for each column, take longest of entry in either dataframe or length of label
     column_widths = [max(get_longest_entry(df1[col]), get_longest_entry(df2[col]), len(col)) for col in column_list]
@@ -44,25 +33,34 @@ def get_column_widths2(df1, df2, column_list):
     return column_widths
 
 
-def convert_df_to_table(df, column_list, column_widths):
-    table = []
+def convert_df_to_table(df, column_list, column_widths, delimiter='|'):
+    """convert dataframe into a delimited text table
+    TODO: convert column_widths to a dictionary to eliminate accessing by index
+    Arguments:
+        df {[type]} -- [description]
+        column_list {[list]} -- list containing names of the columns to be converted. since lists are ordered the columns in resulting table follow order of column_list 
+        column_widths {[list]} -- list or dictionary containing the length of longest entry (or label if label longer than all entries) for this column 
+        delimiter {[list]} -- character or string to be used to delimit/separate columns in table. default delimiter is '|'
+    
+    Returns:
+        [list] -- table as a list. each table row is a separate string
+    """
 
     # header row
-    line = '|'
+    line = delimiter
     for ix, item in enumerate(column_list):
-        line = line + f" {item.ljust(column_widths[ix])} |"
-    # print(f"{line}")
-    table.append(f"{line}\n")
+        line = line + f" {item.ljust(column_widths[ix])} {delimiter}"
+
+    table = [f"{line}\n"]
 
     # data rows
     for _, row in enumerate(df[column_list].itertuples(index=False), 1):
-        line = '|'
+        line = delimiter
         for ix, item in enumerate(list(row)):
-            line = line + f" {str(item).ljust(column_widths[ix])} |"
+            line = line + f" {str(item).ljust(column_widths[ix])} {delimiter}"
 
         table.append(f"{line}\n")
 
-    # return table as list of strings
     return table
 
 def print_table(table, label):
@@ -71,16 +69,27 @@ def print_table(table, label):
         print(f"  {row.rstrip()}")
 
 def compare_tables(expected_df, actual_df, column_list, sort_by=None):
+    """compares two tables and displays the results
+    
+    converts two pandas dataframes to text tables and then compares the tables as lists of strings using difflib
+    Arguments:
+        expected_df {[type]} -- dataframe containing the expected results
+        actual_df {[type]} -- dataframe containing the actual results
+        column_list {[type]} -- list of columns to be used to compare the two tables. all columns in column_list must be present in both tables
+    
+    Keyword Arguments:
+        sort_by {[type]} -- list of columns to be used to sort tables before comparison. if no sort order is specified, all columns are used to sort the tables and column order is defined by column_list paramater
+    """
     if sort_by is None:
         sort_by = column_list
     
-    column_widths = get_column_widths2(df1, df2, column_list)
-    expected = convert_df_to_table(expected_df.sort_values(by=sort_by), column_list, column_widths)
-    actual = convert_df_to_table(actual_df.sort_values(by=sort_by), column_list, column_widths)
+    column_widths = get_column_widths(df1, df2, column_list)
+    expected = convert_df_to_table(expected_df.sort_values(by=sort_by), column_list, column_widths, delimiter='|')
+    actual = convert_df_to_table(actual_df.sort_values(by=sort_by), column_list, column_widths, delimiter='|')
     diff = difflib.ndiff(expected, actual)
 
-    print_table(expected, "expected")
-    print_table(actual, "actual")
+    print_table(table=expected, label="expected")
+    print_table(table=actual, label="actual")
 
     if expected == actual:
         print(f"\ntables match")
